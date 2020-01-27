ed6c8144305ec908afcba037fa33e05aab4cbf3d
Игорь Ильин <igorilin@MacBook-Pro-Igor.local>
2020-01-27 10:58:51 +0300

    Cleanup

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const validateEmail = function(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email)
 };
 
